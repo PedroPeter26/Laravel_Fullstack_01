25f2801b773f5a4c61e78d4e3cb2286ae3280aac
Pedro Hiram Silva S. <silvap1acbta1@gmail.com>
2024-05-14 09:03:32 -0600

    migracion users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,19 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname_p');
+            $table->string('lastname_m');
+            $table->integer('age');
+            $table->date('birthdate');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('phone')->unique();
+            $table->timestamp('email_verified_at')->nullable(); // ! autogenerada
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->boolean('active')->default(false);
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->rememberToken(); // ! autogenerado (quizá relacionado a personal access y password reset)
+            $table->timestamps(); // ! autogenerado
         });
     }
 
